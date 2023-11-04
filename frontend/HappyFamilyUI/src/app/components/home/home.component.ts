d74b93575dfcd0d5886f84e5ad29986ec0e26cf5
Naveen Bathina <naveen.bathina24@outlook.com>
2023-11-05 02:21:52 +0530

    add footer

--- frontend/HappyFamilyUI/src/app/components/home/home.component.ts
+++ frontend/HappyFamilyUI/src/app/components/home/home.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  scrollToElement($element:any): void {
-    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  }
+  currentYear = (new Date()).getFullYear();
 }
